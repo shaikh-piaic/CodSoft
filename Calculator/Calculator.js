039e1fd544e6d65a55d1083737bb3a6d16d00c8b
Shaikh Abdul Sami <108601988+shaikh-piaic@users.noreply.github.com>
2023-09-21 21:41:06 +0500

    Task Three Completed

---
+++ Calculator/Calculator.js
@@ -0,0 +1,30 @@
+let string = "";
+let buttons = document.querySelectorAll(".button");
+
+Array.from(buttons).forEach((item) => {
+    item.addEventListener('click', (x) => {
+        if (x.target.innerHTML == '=') {
+            string = eval(string)
+            document.querySelector(".input-2").value = string;
+
+        }
+        else if (x.target.innerHTML == 'C') {
+            string = "";
+            document.querySelector(".input-1").value = string;
+            document.querySelector(".input-2").value = string;
+
+        }
+        else if (x.target.id == 'backspace') {
+            string = string.slice(0, -1);
+            document.querySelector(".input-1").value = string;
+        }
+        else {
+            console.log(x.target)
+            string += x.target.innerHTML;
+            document.querySelector(".input-1").value = string;
+        }
+    })
+
+
+
+})
